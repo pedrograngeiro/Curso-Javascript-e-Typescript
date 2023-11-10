83c3f0f380e446b81118cd8b50838b8276926f5b
Pedro Grangeiro <pedrop.grangeiro@gmail.com>
2023-11-10 17:15:54 -0300

    Concluido aula 6 e aula 7 modulo js

---
+++ aula6/index.js
@@ -0,0 +1,24 @@
+let nome; // declarou a variável
+
+nome = "João"; // Inicializando a variável
+console.log(nome);
+nome = "Otávio";
+console.log(nome);
+// console.log(nome, "nasceu em 1984");
+// console.log("Em 2000", nome, "conheceu Maria.");
+// console.log(nome, "casou-se com maria em 2012.");
+// console.log("Maria teve 1 filho com", nome, "em 2015");
+// console.log("o filho de", nome, "se chama Eduardo.");
+
+// nao podemos criar variaveis com palavras reservadas
+// variaveis precisam ter nomes significativos
+// Nao pode comecar nome de uma variavel com um numero;
+// Nao pode conter espacos ou tracos;
+// Utilizamos camelCase
+// Case-sensitive
+// Nao podemos redeclarar variaveris com let
+// NAO UTILIZE VAR, UTILIZE LET.
+let nomeCliente = "luiz";
+let nomecliente = "Otavio";
+
+console.log(nomeCliente, nomecliente);
---
+++ aula7/index.js
@@ -0,0 +1,15 @@
+// nao podemos criar constantes com palavras reservadas
+// constantes precisam ter nomes significativos
+// Nao pode comecar nome de uma variavel com um numero;
+// Nao pode conter espacos ou tracos;
+// Utilizamos camelCase
+// Case-sensitive
+// Nao pode modificar o valor de uma constante
+// NAO UTILIZE VAR, UTILIZE CONST.
+
+const primeiroNumero = "5";
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 5;
