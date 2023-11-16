8504e12ba560fd10cf60da892cd9176d4f1feaaa
Pedro Grangeiro <pedrop.grangeiro@gmail.com>
2023-11-16 19:29:29 -0300

    Create index.js

---
+++ aula11/index.js
@@ -0,0 +1,24 @@
+/*
+    Aritméticos
+    + Adição // Contatenacao ( / *)
+    - / * 
+    ** Potenciação
+    % Resto da divisão
+*/
+
+/*/
+    ()
+    **
+    + / %
+    + -
+*/
+
+// const num1 = 5;
+// const num2 = 2;
+// const num3 = 10;
+// console.log((num1 * num2) / num3);
+
+let contador = 10;
+
+console.log(--contador);
+console.log(--contador);
